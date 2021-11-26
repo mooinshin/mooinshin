f04c31abdf7c9ce3f34a726f395196e610b3e7d0
Maks <94012710+mooinshin@users.noreply.github.com>
2021-11-26 21:43:06 +0500

    Delete About_Me.py

--- About_Me.py
+++
@@ -1,7 +0,0 @@
-print(" About me\n- hello my name is Maks\n- i like programming\n-  programming at python\n- i minimal now python\n- i play game\n- play it: CS:GO,Fortnite,Warzone,Raft,Minecraft.\n- my langue are russian\n- my discord is makson_0576 talk with
-
-        Programming code with you <3")
-
-name = input = ("your name?":)
-
-print = ("Okey" + name "go talk to discord")
